0f72ea1a520e4e67a4f4a051282abfde5b818232
Runnerbeany <imalwaysright@virginmedia.com>
2016-10-06 11:48:06 +0100

    Create hello world.py

---
+++ hello world.py
@@ -0,0 +1,4 @@
+name = input("Please, enter your name")
+print("Hello, nice to meet you ",name)
+age = input("How old are you?")
+print("Okay. So your name is", name, "and you are", age, "years old!")
